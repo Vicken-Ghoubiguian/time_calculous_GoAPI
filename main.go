3f9b3a51a8fc080be808be7a830594643a74483e
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-05-16 16:24:35 +0200

    New parameters for urls

--- main.go
+++ main.go
@@ -21,21 +21,25 @@ func main() {
   r.GET("/", func(c *gin.Context) {
 
     //
-    c.JSON(http.StatusOK, gin.H{"data": "hello world"})    
+    c.JSON(http.StatusOK, gin.H{"title": "", "presentation": ""})
   })
 
   //
-  r.GET("/wished_wday_in_choosen_month", func(c *gin.Context) {
+  r.GET("/wished_wday_in_choosen_month/:wday/:month", func(c *gin.Context) {
 
     //
     c.JSON(http.StatusOK, gin.H{"data": "TODO"})
   })
 
   //
-  r.GET("/number_of_weeks_in_a_year_according_to_the_iso_norm", func(c *gin.Context) {
+  r.GET("/number_of_weeks_in_year/:year", func(c *gin.Context) {
+
+    //
+    year := c.Param("year")
+    number_of_weeks := 13
  
     //
-    c.JSON(http.StatusOK, gin.H{"data": "TODO"})
+    c.JSON(http.StatusOK, gin.H{"year": year, "number_of_weeks": number_of_weeks})
   })
 
   //
