e013e530ac3932097ff65a41d6663a27c954a5b0
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-05-19 19:13:58 +0200

    JJKJjj

--- main.go
+++ main.go
@@ -18,21 +18,21 @@ func main() {
 	//
 	r.StaticFile("/favicon.ico", "./resources/favicon.ico")
 
-	//
+	// Definition of the 'root' URL...
 	r.GET("/", func(c *gin.Context) {
 
 		//
 		c.JSON(http.StatusOK, gin.H{"title": "time_calculous_GoAPI", "presentation": "", "description": "", "links": gin.H{"github": "https://github.com/Vicken-Ghoubiguian/time_calculous_GoAPI", "dockerhub": ""}})
 	})
 
-	//
+	// Definition of the 'wished_wday_in_choosen_month' to use the '' function...
 	r.GET("/wished_wday_in_choosen_month/:wday/:month", func(c *gin.Context) {
 
 		//
 		c.JSON(http.StatusOK, gin.H{"data": "TODO"})
 	})
 
-	//
+	// Definition of the 'number_of_weeks_in_year' to use the 'number_of_weeks_in_a_year_according_to_the_iso_norm' function...
 	r.GET("/number_of_weeks_in_year/:year", func(c *gin.Context) {
 
 		//
@@ -43,28 +43,28 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"year": year, "number_of_weeks": number_of_weeks})
 	})
 
-	//
+	// Definition of the 'wished_wday_in_choosen_year' to use the 'wished_wday_in_choosen_year' function...
 	r.GET("/wished_wday_in_choosen_year/:year/:wday/:number_of_weekday", func(c *gin.Context) {
 
 		//
 		c.JSON(http.StatusOK, gin.H{"data": "TODO"})
 	})
 
-	//
+	// Definition of the 'wished_number_in_year_is_day_in_choosen_year' to use the '' function...
 	r.GET("/wished_number_in_year_is_day_in_choosen_year/:mday/:month/:year", func(c *gin.Context) {
 
 		//
 		c.JSON(http.StatusOK, gin.H{"data": "TODO"})
 	})
 
-	//
+	// Definition of the 'calculations_on_date_and_time_from_today' to use the '' function...
 	r.GET("/calculations_on_date_and_time_from_today/:milleniums/:centuries/:decades/:years/:months/:weeks/:days/:hours/:minutes/:seconds", func(c *gin.Context) {
 
 		//
 		c.JSON(http.StatusOK, gin.H{"data": "TODO"})
 	})
 
-	//
+	// Definition of the 'number_of_days_in_choosen_month_in_choosen_year' to use the '' function...
 	r.GET("/number_of_days_in_choosen_month_in_choosen_year/:month/:year", func(c *gin.Context) {
 
 		//
